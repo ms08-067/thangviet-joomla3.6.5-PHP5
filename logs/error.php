e9904914bd64b91def024acb5739e8f7914b5365
msconfig <hunguit@yahoo.com>
2018-02-17 08:13:59 +0700

    Thangviet-joomla3.6.6-PHP5

---
+++ logs/error.php
@@ -0,0 +1,38 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2014-06-24 15:28:01 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority	category	message
+2014-06-24T15:28:01+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-06-24T15:28:16+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-06-24T15:34:47+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-06-24T15:35:04+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-06-24T15:35:45+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-06-24T15:45:05+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-06-30T09:37:43+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-07-30T13:57:31+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-08-10T16:06:42+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-08-10T16:06:50+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2018-02-10T19:34:55+00:00	INFO 113.23.49.155	joomlafailure	Username and password do not match or you do not have an account yet.
+2018-02-10T20:11:31+00:00	INFO 109.228.182.92	joomlafailure	Username and password do not match or you do not have an account yet.
+2018-02-11T08:52:30+00:00	INFO 113.23.49.155	joomlafailure	Username and password do not match or you do not have an account yet.
+2018-02-11T14:23:44+00:00	INFO 185.86.13.213	joomlafailure	Username and password do not match or you do not have an account yet.
+2018-02-14T09:24:05+00:00	INFO 91.210.147.12	joomlafailure	Username and password do not match or you do not have an account yet.
+2018-02-14T10:18:56+00:00	INFO 91.210.147.12	joomlafailure	Username and password do not match or you do not have an account yet.
+2018-02-14T10:28:21+00:00	INFO 91.210.147.12	joomlafailure	Username and password do not match or you do not have an account yet.
+2018-02-14T11:38:05+00:00	INFO 91.210.144.254	joomlafailure	Username and password do not match or you do not have an account yet.
+2018-02-14T11:42:06+00:00	INFO 91.210.144.254	joomlafailure	Username and password do not match or you do not have an account yet.
+2018-02-14T12:52:18+00:00	INFO 91.210.144.254	joomlafailure	Username and password do not match or you do not have an account yet.
+2018-02-14T14:30:56+00:00	INFO 31.184.234.60	joomlafailure	Username and password do not match or you do not have an account yet.
+2018-02-14T23:02:18+00:00	INFO 87.106.48.6	joomlafailure	Username and password do not match or you do not have an account yet.
+2018-02-14T23:31:56+00:00	INFO 87.106.48.6	joomlafailure	Username and password do not match or you do not have an account yet.
+2018-02-14T23:36:52+00:00	INFO 87.106.48.6	joomlafailure	Username and password do not match or you do not have an account yet.
+2018-02-15T00:11:19+00:00	INFO 87.106.48.6	joomlafailure	Username and password do not match or you do not have an account yet.
+2018-02-15T00:13:11+00:00	INFO 87.106.48.6	joomlafailure	Username and password do not match or you do not have an account yet.
+2018-02-15T00:38:07+00:00	INFO 87.106.48.6	joomlafailure	Username and password do not match or you do not have an account yet.
+2018-02-15T08:21:46+00:00	INFO 31.173.82.89	joomlafailure	Username and password do not match or you do not have an account yet.
+2018-02-16T00:40:52+00:00	INFO 80.122.98.69	joomlafailure	Username and password do not match or you do not have an account yet.
+2018-02-16T20:05:38+00:00	INFO 185.212.128.24	joomlafailure	Username and password do not match or you do not have an account yet.
+2018-02-16T23:41:12+00:00	INFO 59.153.242.53	joomlafailure	Username and password do not match or you do not have an account yet.
+2018-02-16T23:41:21+00:00	INFO 59.153.242.53	joomlafailure	Username and password do not match or you do not have an account yet.
---
+++ logs/joomla_update.php
@@ -0,0 +1,67 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2018-02-10 16:19:21 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2018-02-10T16:19:21+00:00	INFO 113.23.49.155	update	COM_JOOMLAUPDATE_UPDATE_LOG_DELETE_FILES
+2018-02-10T16:41:55+00:00	INFO 113.23.49.155	update	COM_JOOMLAUPDATE_UPDATE_LOG_DELETE_FILES
+2018-02-10T16:55:41+00:00	INFO 113.23.49.155	update	Update started by user Super User (880). Old version is 3.4.7.
+2018-02-10T16:55:42+00:00	INFO 113.23.49.155	update	Downloading update file from https://downloads.joomla.org/cms/joomla3/3-6-5/Joomla_3.6.5-Stable-Update_Package.zip.
+2018-02-10T16:56:05+00:00	INFO 113.23.49.155	update	File Joomla_3.6.5-Stable-Update_Package.zip successfully downloaded.
+2018-02-10T16:56:06+00:00	INFO 113.23.49.155	update	Starting installation of new version.
+2018-02-10T16:59:04+00:00	INFO 113.23.49.155	update	Starting installation of new version.
+2018-02-10T17:01:26+00:00	INFO 113.23.49.155	update	Update started by user Super User (880). Old version is 3.4.7.
+2018-02-10T17:01:26+00:00	INFO 113.23.49.155	update	File Joomla_3.6.5-Stable-Update_Package.zip successfully downloaded.
+2018-02-10T17:01:27+00:00	INFO 113.23.49.155	update	Starting installation of new version.
+2018-02-10T17:02:01+00:00	INFO 113.23.49.155	update	Update started by user Super User (880). Old version is 3.4.7.
+2018-02-10T17:02:01+00:00	INFO 113.23.49.155	update	File Joomla_3.6.5-Stable-Update_Package.zip successfully downloaded.
+2018-02-10T17:02:01+00:00	INFO 113.23.49.155	update	Starting installation of new version.
+2018-02-10T17:13:37+00:00	INFO 113.23.49.155	update	Update started by user Super User (880). Old version is 3.4.7.
+2018-02-10T17:13:38+00:00	INFO 113.23.49.155	update	File Joomla_3.6.5-Stable-Update_Package.zip successfully downloaded.
+2018-02-10T17:13:39+00:00	INFO 113.23.49.155	update	Starting installation of new version.
+2018-02-10T17:21:54+00:00	INFO 113.23.49.155	update	Update started by user Super User (880). Old version is 3.4.7.
+2018-02-10T17:21:55+00:00	INFO 113.23.49.155	update	File Joomla_3.6.5-Stable-Update_Package.zip successfully downloaded.
+2018-02-10T17:21:55+00:00	INFO 113.23.49.155	update	Starting installation of new version.
+2018-02-17T00:13:08+00:00	INFO 59.153.242.53	update	Update started by user Super User (880). Old version is 3.4.7.
+2018-02-17T00:13:09+00:00	INFO 59.153.242.53	update	File Joomla_3.6.5-Stable-Update_Package.zip successfully downloaded.
+2018-02-17T00:13:09+00:00	INFO 59.153.242.53	update	Starting installation of new version.
+2018-02-17T00:13:41+00:00	INFO 59.153.242.53	update	Finalising installation.
+2018-02-17T00:13:42+00:00	INFO 59.153.242.53	update	Ran query from file 3.5.0-2015-07-01. Query text: ALTER TABLE `#__session` MODIFY `session_id` varchar(191) NOT NULL DEFAULT '';.
+2018-02-17T00:13:42+00:00	INFO 59.153.242.53	update	Ran query from file 3.5.0-2015-07-01. Query text: ALTER TABLE `#__user_keys` MODIFY `series` varchar(191) NOT NULL;.
+2018-02-17T00:13:42+00:00	INFO 59.153.242.53	update	Ran query from file 3.5.0-2015-10-13. Query text: INSERT INTO `#__extensions` (`extension_id`, `name`, `type`, `element`, `folder`.
+2018-02-17T00:13:42+00:00	INFO 59.153.242.53	update	Ran query from file 3.5.0-2015-10-26. Query text: ALTER TABLE `#__contentitem_tag_map` DROP INDEX `idx_tag`;.
+2018-02-17T00:13:42+00:00	INFO 59.153.242.53	update	Ran query from file 3.5.0-2015-10-26. Query text: ALTER TABLE `#__contentitem_tag_map` DROP INDEX `idx_type`;.
+2018-02-17T00:13:42+00:00	INFO 59.153.242.53	update	Ran query from file 3.5.0-2015-10-30. Query text: UPDATE `#__menu` SET `title` = 'com_contact_contacts' WHERE `client_id` = 1 AND .
+2018-02-17T00:13:42+00:00	INFO 59.153.242.53	update	Ran query from file 3.5.0-2015-11-04. Query text: DELETE FROM `#__menu` WHERE `title` = 'com_messages_read' AND `client_id` = 1;.
+2018-02-17T00:13:42+00:00	INFO 59.153.242.53	update	Ran query from file 3.5.0-2015-11-04. Query text: INSERT INTO `#__extensions` (`extension_id`, `name`, `type`, `element`, `folder`.
+2018-02-17T00:13:42+00:00	INFO 59.153.242.53	update	Ran query from file 3.5.0-2015-11-05. Query text: INSERT INTO `#__extensions` (`extension_id`, `name`, `type`, `element`, `folder`.
+2018-02-17T00:13:42+00:00	INFO 59.153.242.53	update	Ran query from file 3.5.0-2015-11-05. Query text: INSERT INTO `#__postinstall_messages` (`extension_id`, `title_key`, `description.
+2018-02-17T00:13:42+00:00	INFO 59.153.242.53	update	Ran query from file 3.5.0-2016-02-26. Query text: CREATE TABLE IF NOT EXISTS `#__utf8_conversion` (   `converted` tinyint(4) NOT N.
+2018-02-17T00:13:42+00:00	INFO 59.153.242.53	update	Ran query from file 3.5.0-2016-02-26. Query text: INSERT INTO `#__utf8_conversion` (`converted`) VALUES (0);.
+2018-02-17T00:13:42+00:00	INFO 59.153.242.53	update	Ran query from file 3.5.0-2016-03-01. Query text: ALTER TABLE `#__redirect_links` DROP INDEX `idx_link_old`;.
+2018-02-17T00:13:43+00:00	INFO 59.153.242.53	update	Ran query from file 3.5.0-2016-03-01. Query text: ALTER TABLE `#__redirect_links` MODIFY `old_url` VARCHAR(2048) NOT NULL;.
+2018-02-17T00:13:43+00:00	INFO 59.153.242.53	update	Ran query from file 3.5.0-2016-03-01. Query text: ALTER TABLE `#__redirect_links` MODIFY `new_url` VARCHAR(2048);.
+2018-02-17T00:13:44+00:00	INFO 59.153.242.53	update	Ran query from file 3.5.0-2016-03-01. Query text: ALTER TABLE `#__redirect_links` MODIFY `referer` VARCHAR(2048) NOT NULL;.
+2018-02-17T00:13:44+00:00	INFO 59.153.242.53	update	Ran query from file 3.5.0-2016-03-01. Query text: ALTER TABLE `#__redirect_links` ADD INDEX `idx_old_url` (`old_url`(100));.
+2018-02-17T00:13:44+00:00	INFO 59.153.242.53	update	Ran query from file 3.5.1-2016-03-25. Query text: ALTER TABLE `#__user_keys` MODIFY `user_id` varchar(150) NOT NULL;.
+2018-02-17T00:13:44+00:00	INFO 59.153.242.53	update	Ran query from file 3.5.1-2016-03-29. Query text: UPDATE `#__utf8_conversion` SET `converted` = 0  WHERE (SELECT COUNT(*) FROM `#_.
+2018-02-17T00:13:44+00:00	INFO 59.153.242.53	update	Ran query from file 3.6.0-2016-04-01. Query text: UPDATE `#__update_sites` SET `name` = 'Joomla! Core' WHERE `name` = 'Joomla Core.
+2018-02-17T00:13:44+00:00	INFO 59.153.242.53	update	Ran query from file 3.6.0-2016-04-01. Query text: UPDATE `#__update_sites` SET `name` = 'Joomla! Extension Directory' WHERE `name`.
+2018-02-17T00:13:44+00:00	INFO 59.153.242.53	update	Ran query from file 3.6.0-2016-04-01. Query text: UPDATE `#__update_sites` SET `location` = 'https://update.joomla.org/core/list.x.
+2018-02-17T00:13:44+00:00	INFO 59.153.242.53	update	Ran query from file 3.6.0-2016-04-01. Query text: UPDATE `#__update_sites` SET `location` = 'https://update.joomla.org/jed/list.xm.
+2018-02-17T00:13:44+00:00	INFO 59.153.242.53	update	Ran query from file 3.6.0-2016-04-01. Query text: UPDATE `#__update_sites` SET `location` = 'https://update.joomla.org/language/tr.
+2018-02-17T00:13:44+00:00	INFO 59.153.242.53	update	Ran query from file 3.6.0-2016-04-01. Query text: UPDATE `#__update_sites` SET `location` = 'https://update.joomla.org/core/extens.
+2018-02-17T00:13:44+00:00	INFO 59.153.242.53	update	Ran query from file 3.6.0-2016-04-06. Query text: ALTER TABLE `#__redirect_links` MODIFY `new_url` VARCHAR(2048);.
+2018-02-17T00:13:44+00:00	INFO 59.153.242.53	update	Ran query from file 3.6.0-2016-04-08. Query text: INSERT INTO `#__extensions` (`extension_id`, `name`, `type`, `element`, `folder`.
+2018-02-17T00:13:44+00:00	INFO 59.153.242.53	update	Ran query from file 3.6.0-2016-04-08. Query text: UPDATE `#__update_sites_extensions` SET `extension_id` = 802 WHERE `update_site_.
+2018-02-17T00:13:44+00:00	INFO 59.153.242.53	update	Ran query from file 3.6.0-2016-04-09. Query text: ALTER TABLE `#__menu_types` ADD COLUMN `asset_id` INT(11) NOT NULL AFTER `id`;.
+2018-02-17T00:13:44+00:00	INFO 59.153.242.53	update	Ran query from file 3.6.0-2016-05-06. Query text: DELETE FROM `#__extensions` WHERE `type` = 'library' AND `element` = 'simplepie'.
+2018-02-17T00:13:45+00:00	INFO 59.153.242.53	update	Ran query from file 3.6.0-2016-05-06. Query text: INSERT INTO `#__extensions` (`extension_id`, `name`, `type`, `element`, `folder`.
+2018-02-17T00:13:45+00:00	INFO 59.153.242.53	update	Ran query from file 3.6.0-2016-06-01. Query text: UPDATE `#__extensions` SET `protected` = 1, `enabled` = 1  WHERE `name` = 'com_a.
+2018-02-17T00:13:45+00:00	INFO 59.153.242.53	update	Ran query from file 3.6.0-2016-06-05. Query text: ALTER TABLE `#__languages` ADD COLUMN `asset_id` INT(11) NOT NULL AFTER `lang_id.
+2018-02-17T00:13:46+00:00	INFO 59.153.242.53	update	Ran query from file 3.6.3-2016-08-15. Query text: ALTER TABLE `#__newsfeeds` MODIFY `link` VARCHAR(2048) NOT NULL;.
+2018-02-17T00:13:46+00:00	INFO 59.153.242.53	update	Ran query from file 3.6.3-2016-08-16. Query text: INSERT INTO `#__postinstall_messages` (`extension_id`, `title_key`, `description.
+2018-02-17T00:13:46+00:00	INFO 59.153.242.53	update	Deleting removed files and folders.
+2018-02-17T00:14:14+00:00	INFO 59.153.242.53	update	Cleaning up after installation.
+2018-02-17T00:14:14+00:00	INFO 59.153.242.53	update	Update to version 3.6.5 is complete.
